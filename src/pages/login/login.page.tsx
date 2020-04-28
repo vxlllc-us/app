e1d18f6a44957d9ac7d1aac2f99738e1844313fb
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-04-29 00:01:28 +0530

    added login logic

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,11 +1,117 @@
 import React from "react";
+import { ScaleLoader } from "react-spinners";
+import { auth, UserInfo } from "firebase";
 
-export default class Login extends React.Component {
+import "./login.scss";
+import { theme } from "../../res";
+import * as wrapperModules from "../wrapper/wrapper.module";
+
+const google = require("../../res/assets/images/logos/google.png");
+const logo = require("../../res/assets/images/logos/logo-black.png");
+
+interface State {
+  loading: boolean;
+  user: UserInfo | null;
+}
+class Login extends React.Component<any, State> {
+  state: State = {
+    loading: false,
+    user: null
+  };
+
+  async componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    wrapperModules.setup();
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          user
+        });
+      }
+      this.setState({
+        loading: false
+      });
+    });
+  }
+
+  onLogin = () => {
+    const provider = new auth.GoogleAuthProvider();
+    auth()
+      .signInWithPopup(provider)
+      .then((result: any) => {
+        const token = result.credential.accessToken;
+        const user: UserInfo = result.user;
+        console.log("token: ", token);
+        console.log("user: ", user);
+        this.setState({
+          loading: false,
+          user
+        });
+      })
+      .catch(error => {
+        console.log("error: ", error);
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = error.credential;
+        this.setState({
+          loading: false
+        });
+      });
+  };
+
+  onLogout = () => {
+    this.setState({
+      loading: true
+    });
+    auth()
+      .signOut()
+      .then(() => {
+        this.setState({
+          loading: false,
+          user: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          loading: false
+        });
+      });
+  };
+
   render() {
     return (
-      <div className="container">
-        <h1>Login</h1>
+      <div className="login-root">
+        <div className="container">
+          <img className={"logo"} src={logo} />
+          {this.state.loading && (
+            <ScaleLoader
+              color={theme.secondary}
+              height={12}
+              width={4}
+              radius={2}
+              margin={2}
+            />
+          )}
+          {!this.state.loading && this.state.user && (
+            <>
+              <button className={"logout"} onClick={this.onLogout}>
+                Logout
+              </button>
+            </>
+          )}
+          {!this.state.loading && !this.state.user && (
+            <button className={"login"} onClick={this.onLogin}>
+              <img src={google} />
+              <h4>Login with Google</h4>
+            </button>
+          )}
+        </div>
       </div>
     );
   }
 }
+
+export default Login;
--- src/pages/wrapper/wrapper.module.ts
+++ src/pages/wrapper/wrapper.module.ts
@@ -12,13 +12,15 @@ const {
 } = process.env;
 
 export function setup() {
-  firebase.initializeApp({
-    apiKey: REACT_APP_API_KEY,
-    authDomain: REACT_APP_AUTH_DOMAIN,
-    databaseUrl: REACT_APP_DATABASE_URL,
-    projectId: REACT_APP_PROJECT_ID,
-    storageBucket: REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-    appId: REACT_APP_APP_ID
-  });
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: REACT_APP_API_KEY,
+      authDomain: REACT_APP_AUTH_DOMAIN,
+      databaseUrl: REACT_APP_DATABASE_URL,
+      projectId: REACT_APP_PROJECT_ID,
+      storageBucket: REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+      appId: REACT_APP_APP_ID
+    });
+  }
 }
